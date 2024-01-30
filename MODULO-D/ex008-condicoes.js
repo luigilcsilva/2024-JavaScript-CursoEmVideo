c8d675e0e6410bdc0e44133d95ca411f864ffe68
Luigi Lucas de Carvalho Silva <luigi.lcsilva@gmail.com>
2024-01-30 15:42:21 -0300

    Adding some files.

---
+++ MODULO-D/ex008-condicoes.js
@@ -0,0 +1,13 @@
+var vel = 40
+console.log(`A velocidade do seu carro é ${vel} Km/h.`)
+if (vel > 60) {               //Condição simples.
+    console.log('Multado.')
+}
+
+var país = 'Brasil'
+console.log(`Vivendo em ${país}.`)
+if (país == 'Brasil') {
+    console.log('Brasileiro.')
+} else {
+    console.log ('Estrangeiro.')
+}
