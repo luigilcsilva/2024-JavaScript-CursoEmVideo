d0c2b1f4ce52a9a45efd15096cdb977fc5adcd6d
Luigi Lucas de Carvalho Silva <luigi.lcsilva@gmail.com>
2024-02-02 14:22:49 -0300

    Adding files.

---
+++ MODULO-E/ex015.js
@@ -0,0 +1,32 @@
+//var c = 0
+
+//Estrutura de repetição com teste lógico no início.
+/*
+while (c < 4) {
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1
+}
+*/
+
+//Estrutura de repetição com teste lógico no final.
+/*
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 2)
+*/
+
+//Loop do tipo for
+/*
+for (var c = 0; c < 6;c++) {
+    console.log(c)
+}
+*/
+
+//Modo depuração
+console.log('Vai começar...')
+for (var c = 1; c <= 4;c++) {
+    console.log(c)
+}
+console.log('FIM!')
+
